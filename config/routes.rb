a6fbf6314076252da8bf0245de6fcfcce78333e4
vagrant <vagrant@precise64.(none)>
2018-08-01 20:11:37 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-
   root 'static_pages#home'
-
-  get 'static_pages/home'
-
-  get 'static_pages/sign_in'
-
-  get 'static_pages/sign_up'
-
+  get '/sign_in', to: 'static_pages#sign_in'
+  get '/sign_up', to: 'teachers#new'
+  # get '/sign_up', to: 'static_pages#sign_up'
 end
+
+# Rails.application.routes.draw do
+  # get 'teachers/new'
+
+#
+#   root 'static_pages#home'
+#
+#   get 'static_pages/home'
+#
+#   get 'static_pages/sign_in'
+#
+#   get 'static_pages/sign_up'
+#
+# end
