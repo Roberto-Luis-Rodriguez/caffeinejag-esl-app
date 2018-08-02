bf427df2acece47db72c7c8f1bf570ee96300420
vagrant <vagrant@precise64.(none)>
2018-08-02 15:34:54 +0000

    Modify _header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/sign_in', to: 'static_pages#sign_in'
   get '/sign_up', to: 'teachers#new'
-  # get '/sign_up', to: 'static_pages#sign_up'
 end
 
 # Rails.application.routes.draw do
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,26 +1,18 @@
-# require 'rails_helper'
-#
-# RSpec.describe StaticPagesController, type: :controller do
-#
-#   describe "GET #home" do
-#     it "returns http success" do
-#       get :home
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-#
-#   describe "GET #sign_in" do
-#     it "returns http success" do
-#       get :sign_in
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-#
-#   describe "GET #sign_up" do
-#     it "returns http success" do
-#       get :sign_up
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-#
-# end
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #sign_in" do
+    it "returns http success" do
+      get :sign_in
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
