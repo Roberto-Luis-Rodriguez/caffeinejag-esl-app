409ebc3cd870e1a647ef0a7108a95daded86c313
vagrant <vagrant@precise64.(none)>
2018-08-15 14:28:42 +0000

    alter page elements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
   def create
    teacher = Teacher.find_by(email: params[:session][:email].downcase)
    if teacher && teacher.authenticate(params[:session][:password])
+     # Log the user in and redirect to the user's show page.
      sign_in teacher
      redirect_to teacher
-     # Log the user in and redirect to the user's show page.
    else
      # Create an error message.
      flash.now[:danger] = 'Invalid email/password combination'
--- spec/features/02_teacher_signs_in_spec.rb
+++ spec/features/02_teacher_signs_in_spec.rb
@@ -1,42 +1,39 @@
 require 'rails_helper'
 #
-feature 'Spec for Sign in', %q(
-As an unauthenticated teacher
-I want to sign in
-So that I can see, upload and/or
-delete my lesson plans,
-flashcards, videos and worksheets
-) do
-#
+# feature 'Spec for Sign in', %q(
+# As an unauthenticated teacher
+# I want to sign in
+# So that I can see, upload and/or
+# delete my lesson plans,
+# flashcards, videos and worksheets
+# ) do
+# #
 # #Acceptance Criteria
 # # If I input a registered email and password -> I sign in
 # # If I input an unregistered email and password or
 # # wrong email and/or password -> I can't sign in
 # # If I am already signed in -> I can't again sign in
 #
-# def setup
-#   @teacher = teachers(:roberto)
+
+# scenario 'an existing teacher specifies a valid email and password' do
+#    teacher = FactoryBot.create(:teacher)
+#    visit sign_in_path
+#    fill_in "Email", with: teacher.email
+#    fill_in "Password", with: teacher.password
+# 	 fill_in "Confirmation", with: teacher.password
+#    click_button "Sign in"
+#  end
 # end
 
-scenario 'an existing teacher specifies a valid email and password' do
-   teacher = FactoryBot.create(:teacher)
-   visit sign_in_path
-   fill_in "Email", with: teacher.email
-   fill_in "Password", with: teacher.password
-	 fill_in "Confirmation", with: teacher.password
-   click_button "Sign in"
-   # expect(page).to have_content "Welcome to the CaffeineJag Esl App!"
- end
-end
-
  # scenario 'a non-existent email and password is supplied' do
- #   user = FactoryBot.create(:user)
- #   visit new_user_session_path
- #   fill_in "Email", with: user.email
- #   fill_in "Password", with: user.password
+ #   teacher = FactoryBot.create(:teacher)
+ #   visit sign_in_path
+ #   fill_in "Email", with: teacher.email
+ #   fill_in "Password", with: teacher.password
+ #   fill_in "Confirmation", with: 'incorrectPassword'
  #   click_button "Sign in"
- #   expect(page).to have_content ""
-   # expect(page).to_not have_content ""
+   # expect(page).to have_content "Invalid email/password combination"
+ #  end
  # end
 
  # scenario 'an existing email with the wrong password is denied access' do
@@ -45,7 +42,7 @@ end
  #  fill_in "Email", with: user.email
  #  fill_in "Password", with: 'incorrectPassword'
  #  click_button "Sign in"
- #  expect(page).to have_content ""
+  # expect(page).to have_content ""
  # expect(page).to_not have_content ""
  # end
 
