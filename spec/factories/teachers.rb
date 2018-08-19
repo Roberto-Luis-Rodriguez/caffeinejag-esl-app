efb4b070d818159b7d70cee32ed9d7eacc631723
vagrant <vagrant@precise64.(none)>
2018-08-19 15:41:48 +0000

    updating-teachers

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :teacher do
-    name "Anything"
-    email "anything@example.com"
-    password '1234567'
-    password_confirmation '1234567'
+    name {"Anything"}
+    email {"anything@example.com"}
+    password {'1234567'}
+    password_confirmation {'1234567'}
   end
 end
