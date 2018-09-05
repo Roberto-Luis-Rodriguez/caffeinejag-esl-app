3228071e3224e0126b24104ae9bbb5633865f0dd
vagrant <vagrant@precise64.(none)>
2018-09-05 17:41:56 +0000

    Resolve HomePage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'intense-dawn-64371.herokuapp.com/'
+  host = 'https://intense-dawn-64371.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
