c8604d570c4d5458dad434e15b64925a9aa4d5d4
vagrant <vagrant@precise64.(none)>
2018-10-08 16:40:34 +0000

    Add fog gem

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,14 +5,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+     storage :fog
+   else
+     storage :file
+   end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
