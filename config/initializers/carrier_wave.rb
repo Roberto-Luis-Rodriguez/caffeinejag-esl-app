3cc3b6dbd3fb39ebbb54cd19f05164ebfbd41624
vagrant <vagrant@precise64.(none)>
2018-10-09 13:51:03 +0000

    configure carrierwave for production2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,23 +1,23 @@
-# CarrierWave.configure do |config|
-#   if !Rails.env.test?
-#     config.fog_credentials = {
-#       # Configuration for Amazon S3
-#       provider: "AWS",
-#         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#         aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-#       }
-#       config.fog_directory  = ENV["S3_BUCKET"]
-#     end
-#   end
-
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if !Rails.env.test?
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
+      provider: "AWS",
+        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      }
+      config.fog_directory  = ENV["S3_BUCKET"]
+    end
   end
-end
+
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_credentials = {
+#       # Configuration for Amazon S3
+#       :provider              => 'AWS',
+#       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+#       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+#     }
+#     config.fog_directory     =  ENV['S3_BUCKET']
+#   end
+# end
